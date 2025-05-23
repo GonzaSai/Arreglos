8275e2863ecff469493af1b87d544cd8ba6aa48c
GonzaSai <49086284@ivt.edu.ar>
2025-05-23 14:35:15 -0300

    Arreglos

--- script.js
+++ script.js
@@ -34,3 +34,19 @@ console.log(colores[0])
 
 colores[1] = 'marron'
 console.log(colores[1])
+
+//9)
+
+for (let indice = 0; indice < colores.length; indice++) {
+    console.log(colores[indice])
+    
+}
+
+//10)
+
+let color1 = prompt('Introduce un color')
+let color2 = prompt('Introduce un color')
+let color3 = prompt('Introduce un color')
+let nuevosColores = [color1, color2, color3]
+console.log(nuevosColores)
+
